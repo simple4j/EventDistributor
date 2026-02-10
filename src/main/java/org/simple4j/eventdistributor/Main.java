9d3d0b56b8318d2ea1ba73d1b820391b7e63f88e
Srinivas Janakiraman <jsrinivas@gmail.com>
2026-02-09 21:02:18 -0500

    added uniform handling of ZonedDateTime

--- src/main/java/org/simple4j/eventdistributor/Main.java
+++ src/main/java/org/simple4j/eventdistributor/Main.java
@@ -26,6 +26,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -43,6 +44,7 @@ public class Main
     private static final ObjectMapper OBJECT_MAPPER = JsonMapper.builder()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .serializationInclusion(Include.NON_NULL)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .build();
     private static final String REQUEST_ID_KEY = "requestId";
     private static final String JAPI_RETURN_OBJECT = "returnObject";
@@ -298,6 +300,8 @@ public class Main
             
             HashMap<String, List<Event>> retMap = new HashMap<String, List<Event>>();
             retMap.put("events", ret.responseObject);
+            System.out.println("retMap="+retMap);
+            System.out.println("JSONretMap="+OBJECT_MAPPER.writeValueAsString(retMap));
             ctx.result(OBJECT_MAPPER.writeValueAsString(retMap));
         });
 
