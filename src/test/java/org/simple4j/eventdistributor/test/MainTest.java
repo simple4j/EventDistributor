11be03ed0e733c414de30badcec284ff1888365f
Srinivas Janakiraman <jsrinivas@gmail.com>
2026-05-02 15:12:28 -0400

    Changes to spawn WireMock as separate JVM process to remove interdepencency between Javalin and WireMock on Jetty

--- src/test/java/org/simple4j/eventdistributor/test/MainTest.java
+++ src/test/java/org/simple4j/eventdistributor/test/MainTest.java
@@ -1,9 +1,15 @@
 package org.simple4j.eventdistributor.test;
 
 
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -22,26 +28,46 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.google.common.collect.Lists;
+//import com.google.common.collect.Lists;
 
 public class MainTest
 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-	static WireMockServer wm1 = null;
-	static WireMockServer wm2 = null;
-	
+
+	static String wm1Port="2001";
+	static String wm1RootDir = "/wiremock1";
+	static String wm2Port="2002";
+	static String wm2RootDir = "/wiremock2";
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception
 	{
-		wm1 = new WireMockServer(WireMockConfiguration.options().bindAddress("localhost").port(2001).withRootDirectory(MainTest.class.getResource("/wiremock1").getPath()));
-		wm1.start();
-		wm2 = new WireMockServer(WireMockConfiguration.options().bindAddress("localhost").port(2002).withRootDirectory(MainTest.class.getResource("/wiremock2").getPath()));
-		wm2.start();
+
+		startWireMockService(wm1Port, wm1RootDir);
+
+		startWireMockService(wm2Port, wm2RootDir);
+
 		Main.main(new String[]{"true"});
 		exposePOJOAsHTTPService();
+	}
+
+	private static void startWireMockService(String port, String rootDir)
+			throws IOException
+	{
+		String buildDir = System.getProperty("buildDir");
+		String wiremockjar = System.getProperty("wiremockjar");
+		String buildTestOutputDirectory = System.getProperty("buildTestOutputDirectory");
+		
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", buildDir+"/"+wiremockjar,
+				"--disable-gzip", "true", "--bind-address", "localhost", "--port", port,
+				"--root-dir", buildTestOutputDirectory+rootDir, "--verbose");
+		
+		// Merge stderr into stdout, then inherit
+		pb.redirectErrorStream(true);   // merges stderr into stdout
+		pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+		Process process = pb.start();
 	}
 
 	private static void exposePOJOAsHTTPService()
@@ -49,8 +75,9 @@ public class MainTest
 		ApplicationContext ac = Main.getContext();
 		Object bean = ac.getBean("eventDistributorMapper");
 		Class<? extends Object> class1 = bean.getClass();
-		LOGGER.info("getting method instance from object of classes: {}", Lists.asList(Class.class, class1.getClasses()));
-		LOGGER.info("declared methods are: {}", Lists.asList(Method.class, class1.getDeclaredMethods()));
+		
+		LOGGER.info("getting method instance from object of classes: {}", Arrays.asList(class1.getClasses()));
+		LOGGER.info("declared methods are: {}", Arrays.asList(class1.getDeclaredMethods()));
 
 		JavalinHTTPExposer javalinHTTPExposer = new JavalinHTTPExposer(ac);
 		javalinHTTPExposer.setListenerPortNumber(2411);
@@ -60,10 +87,37 @@ public class MainTest
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception
 	{
-		if(wm1 != null)
-			wm1.shutdownServer();
-		if(wm2 != null)
-			wm2.shutdownServer();
+		shutdownWiremock(wm1Port);
+		shutdownWiremock(wm2Port);
+	}
+
+	private static void shutdownWiremock(String port)
+	{
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        String json = "";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:"+port+"/__admin/shutdown"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        HttpResponse<String> response;
+		try
+		{
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			LOGGER.info("Status : " + response.statusCode());
+			LOGGER.info("Body   : " + response.body());		
+		} catch (IOException e)
+		{
+			LOGGER.warn("",e);
+		} catch (InterruptedException e)
+		{
+			LOGGER.warn("",e);
+		}
+
 	}
 
 	@Before
